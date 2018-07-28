250f9ed323f991c1ceac931ce1773811ed93b519
rex_yan <rex_yan@126.com>
2018-07-29 00:30:52 +0800

    使用vue-cli，iview搭建简单的项目

---
+++ vue-cli/OHC-CMS/src/components/Home.vue
@@ -0,0 +1,86 @@
+<style scoped>
+.layout{
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
+}
+.layout-logo{
+    width: 100px;
+    height: 30px;
+    background: #5b6270;
+    border-radius: 3px;
+    float: left;
+    position: relative;
+    top: 15px;
+    left: 20px;
+}
+.layout-nav{
+    width: 420px;
+    margin: 0 auto;
+    margin-right: 20px;
+}
+.layout-footer-center{
+    text-align: center;
+}
+</style>
+<template>
+    <div class="layout">
+        <Layout>
+            <Header>
+                <Menu mode="horizontal" theme="dark" active-name="1">
+                    <div class="layout-logo"></div>
+                    <div class="layout-nav">
+                        <MenuItem name="1">
+                            <Icon type="ios-navigate"></Icon>
+                            登录
+                        </MenuItem>
+                        <MenuItem name="2">
+                            <Icon type="ios-keypad"></Icon>
+                            注册
+                        </MenuItem>
+                        <MenuItem name="3">
+                            <Icon type="ios-analytics"></Icon>
+                            打榜
+                        </MenuItem>
+                        <MenuItem name="4">
+                            <Icon type="ios-paper"></Icon>
+                            事实信息
+                        </MenuItem>
+                    </div>
+                </Menu>
+            </Header>
+            <Content :style="{padding: '0 50px'}">
+                <Breadcrumb :style="{margin: '20px 0'}">
+                    <BreadcrumbItem>Home</BreadcrumbItem>
+                </Breadcrumb>
+                <Card>
+                    <div style="min-height: 200px;">
+                      <!--主体内容部分-->
+                      <Register></Register>
+
+                    </div>
+                </Card>
+            </Content>
+            <Footer class="layout-footer-center">{{ footer_info }}</Footer>
+        </Layout>
+    </div>
+</template>
+
+
+<script>
+  import Register from './Register.vue'
+  export default {
+    name: 'home',
+    components: {Register},
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        footer_info:'2011-2016 &copy; OurHome-SH',
+        Cotent:"adadadad"
+      }
+    }
+  }
+</script>
+
